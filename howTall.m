df21d92a4609bddb6c025ad779dfa77a43e90bdf
Kenny Robinson <robinsonk2@wit.edu>
2019-04-15 20:06:02 -0400

    Person Detection and Webcam capability
    Disables the sift matcher
    
    Adds person detection, returns a measurement of height of detected rectangle (may need to subtract some buffer space during calculation)
    
    Adds Webcam capability to UI (external USB webcam)

--- howTall.m
+++ howTall.m
@@ -1,10 +1,11 @@
 function [outputImage, heightString] = howTall(inputImage)
     input = imread(inputImage);
+    inputJPG = input;
     imwrite(input,'input.pgm');
     input = imread('input.pgm');
     id = imread('idphoto.jpg');
     imwrite(id,'idphoto.pgm');
-    match('input.pgm', 'idphoto.pgm');
+    input = imrotate(input, 270);
     
     %Image thresolding stuff
     bw = imbinarize(input);
@@ -20,6 +21,13 @@ function [outputImage, heightString] = howTall(inputImage)
     imshow(bw);
     [B,L] = bwboundaries(bw,'noholes');
     
+    peopleDetector = vision.PeopleDetector;
+    [bboxes, scores] = peopleDetector(input);
+    foundPeople = insertObjectAnnotation(input,'rectangle',bboxes, scores);
+    figure;
+    imshow(foundPeople);
+    title('Detected people and detection scores');
+    height = bboxes(4);
     
     %image boundaries from matlab forums
     axis image; % Make sure image is not artificially stretched because of screen's aspect ratio.
@@ -41,8 +49,8 @@ function [outputImage, heightString] = howTall(inputImage)
 %         boundary = B(k);
 %         plot(boundary(:,2),boundary(:,1),'w','LineWidth',2)
 %     end
-    outputImage = inputImage;
-    heightString = "This person is very tall";
+    outputImage = foundPeople;
+    heightString = "This person is " + height +" pixels tall";
 
 %https://www.mathworks.com/help/images/identifying-round-objects.html#d120e26688
 %https://www.mathworks.com/matlabcentral/answers/116793-how-to-classify-shapes-of-this-image-as-square-rectangle-triangle-and-circle
